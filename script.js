399d9300b20a70524ef5c3196a588d4a1dc9518c
David <dgraciano8424@gmail.com>
2026-09-10 20:03:50 -0700

    123

--- script.js
+++ script.js
@@ -7,36 +7,45 @@ let submitPost = document.getElementsByClassName("submit-post")[0];
 
 // * let body = document.getElementsByClassName("body");
 let commentList = document.getElementsByClassName("commentList")[0];
+
 // * comment text input
 let commentText = document.getElementsByClassName("commentInputText")[0];
+
 // * comment name input
 let commentName = document.getElementsByClassName("commentInputName")[0];
+
 // * not sure if I need
 let allPostArr = [];
+
 // * holds comments
 let comArr = [];
+
 // * holds posts
 let postArr = [];
+
 // * Post Text Input
 let postText = document.getElementsByClassName("postInputText")[0];
+
 // * Post Name Input
 let postName = document.getElementsByClassName("postInputName")[0];
 
 // * Post Container
 let postList = document.getElementsByClassName("postList")[0];
 
+// * Remove Button
+let contentContainer = document.getElementsByClassName("contentContainer")[0];
+
+contentContainer.addEventListener("click", function (e) {
+  if (e.target.closest(".remove")) {
+  }
+});
+
 // * Submit Comment button
 let subComBTN = submitComment.addEventListener("click", function () {
-  // * console.log(commentText, "hello");
-
-  // * if (commentText.value == "" || commentName.value == "") {
-
-  // *   alert "hello"
-  // * };
   let comment = {
     text: commentText.value,
     name: commentName.value,
-    comments: comArr
+    comments: comArr,
   };
 
   comArr.push(comment);
@@ -51,7 +60,7 @@ let subPostBTN = submitPost.addEventListener("click", function () {
   let post = {
     text: postText.value,
     name: postName.value,
-    comments: postArr
+    comments: postArr,
   };
 
   postArr.push(post);
@@ -66,10 +75,11 @@ let renderPost = function () {
 
   let commentsHTML = "";
   for (var i = 0; i < comArr.length; i++) {
-    commentsHTML += "<div>" + "<button class='sd'>" + "remove " + " </button>" + " <button> </button>" + comArr[i].text + "  Posted by:  " + comArr[i].name + "</div>";
+    commentsHTML +=
+      "<div>" + "<button class='remove' data-name= + "'" + comment-index  + "'" + > + "remove " + " </button>" + " <button> </button>" + comArr[i].text + "  Posted by:  " + comArr[i].name + "</div>";
   }
   commentList.innerHTML = commentsHTML;
-let removeBTN = 
+
   // * Post loop
   let postHTML = "";
 
